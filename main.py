64e4ef5a4fe256b9607e2e4e946f1187d441202f
greenylox <marcel.ploechl@loxone.com>
2024-10-18 11:36:26 +0200

    updated main.py + added raw.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # EASY TEMPLATE TO START WITH PY-CORD
-import discord                                          #insert this to your terminal: pip install py-cord
-from discord.ext import tasks, commands
-import os
-import random
 
+
+# These are your imports
+# For "Py-Cord" you only have to import discord and commands
+# Official Py-Cord Guide: https://guide.pycord.dev/category/getting-started
+import discord                                          #insert this to your terminal: pip install py-cord
+from discord.ext import commands
+
+
+
+# These are your intents
+# You're defining the categories your bot can interact with
 intents = discord.Intents.default()
-intents.message_content = True
-intents.guilds = True
-intents.members = True
-intents.messages = True
-intents.presences = True
+intents.message_content = True # Access to messages on the server
+intents.guilds = True # Access to guilds on the server
+intents.members = True # Access to members on the server
 
 
 # configure your bot. You'll need it for all commands and ui components
@@ -20,51 +25,22 @@ bot = commands.Bot(command_prefix='!bot', intents=intents)
 # Send a message in your console when the bot is ready.
 @bot.event
 async def on_ready():
-    change_status.start()
     latency_rounded = round(bot.latency, 2)
-    print(f"❱ Bot started as:")
+    print(f"❱ Started as:")
     print(f"『 {bot.user.name} 』")
     print("")
     print("❱ Ping:")
-    print(f"『 {latency_rounded}ms 』")
+    print(f"『 {latency_rounded}s 』")
+    await bot.change_presence(activity=discord.CustomActivity("Bot is online! :D"), status=discord.Status.online)
 
 
-# Auto-changing status for your bot
-@tasks.loop(seconds=50) 
-async def change_status():
-    serverid = int(1177508137511694357) # put in correct server id!
-    # Check server ID with "guild"
-    guild = bot.get_guild(serverid)
-    if guild:
-        # Count server members
-        member_count = guild.member_count
-        # Get latency in seconds
-        latency_rounded = round(bot.latency, 2)
-    else:
-        print("Wrong server ID! (404 not found) => line 35")
-        
-    # Here you can put in your custom message that should be displayed
-    status_messages = [
-    "Playing with admin commands", 
-    "Made with 🤍 by @greenysoka", 
-    f"Ping: {latency_rounded}s", 
-    "eating pizza",
-    f"chatting with {member_count} users"
-]
-    new_status = random.choice(status_messages)
-    # Set the status for the bot
-    await bot.change_presence(activity=discord.CustomActivity(new_status), status=discord.Status.online)
-
-
-
-        ##############################
 
 # define ping slash command
-@bot.slash_command(name="ping", description="Check the ping for the template bot!")
+@bot.slash_command(name="ping", description="Check the ping for your new bot!")
 # define async function
 async def pingcmd(ctx):
     # reply with the latency of the bot
-    await ctx.respond(f"Bot is online! > Ping: {bot.latency}s")
+    await ctx.respond(f"Congrats, bot is online! > Ping: {bot.latency}s")
 
         ##############################
 
---
+++ raw.py
@@ -0,0 +1,28 @@
+# This is the main.py without comments:
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!bot', intents=intents)
+
+@bot.event
+async def on_ready():
+    latency_rounded = round(bot.latency, 2)
+    print(f"❱ Started as:")
+    print(f"『 {bot.user.name} 』")
+    print("")
+    print("❱ Ping:")
+    print(f"『 {latency_rounded}s 』")
+    await bot.change_presence(activity=discord.CustomActivity("Bot is online! :D"), status=discord.Status.online)
+
+@bot.slash_command(name="ping", description="Check the ping for your new bot!")
+async def pingcmd(ctx):
+    await ctx.respond(f"Congrats, bot is online! > Ping: {bot.latency}s")
+
+token = "abc"
+bot.run(token)
